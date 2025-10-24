2daa78d5d5f092f2cff52f44d210d1abdc4df830
talalcodes55 <talalalghazal55@gmail.com>
2025-10-24 17:42:08 +0300

    created the app header

---
+++ components/AppHeader.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+import { Navbar } from './Navbar'
+import Searchbar from './Searchbar'
+import { Button } from './ui/button'
+import 'boxicons/css/boxicons.min.css'
+export default function AppHeader() {
+    return (
+        <header className='px-6 py-3 flex items-center justify-evenly fixed w-full border-b'>
+            <Link href='/' className='font-bold text-2xl'>Store<span className='text-amber-500'>Brand.</span></Link>
+            <Navbar />
+            <Searchbar />
+            <div className="flex gap-7">
+                <Button size={'icon-lg'} className='rounded-full cursor-pointer bg-transparent hover:bg-muted'><i className='bx bx-cart text-2xl text-black' ></i></Button>
+                <Button size={'icon-lg'} className='rounded-full cursor-pointer bg-transparent hover:bg-muted'><i className='bx bx-user text-2xl text-black' ></i></Button>
+            </div>
+        </header>
+    )
+}
