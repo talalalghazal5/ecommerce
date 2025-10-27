e77ca80ecaf141032c596af20f6cb6be1aa241ad
talalcodes55 <talalalghazal55@gmail.com>
2025-10-27 11:57:40 +0300

    changed the size of the hero button

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
           <h1 className="text-5xl font-bold">Shopping became <span className="font-rouge text-7xl text-amber-500">premium.</span></h1>
             <h2 className="mt-3 mb-4 max-w-2xl text-md text-gray-700">
             Discover a truly elevated shopping experience — curated collections handpicked for quality and style, effortless one-click checkout.
-            </h2>
-            <Button className="bg-blue-700 cursor-pointer hover:bg-blue-900 text-sm" size={"lg"}>Go shopping</Button>
+          </h2>
+          <Button className="bg-blue-700 cursor-pointer hover:bg-blue-900 text-sm" size={"lg"}>Go shopping</Button>
         </div>
         <div className="relative">
           <Image src={heroImage} alt="hero image" width={400} height={400} className="relative"></Image>
