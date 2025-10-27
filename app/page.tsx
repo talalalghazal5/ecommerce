63f47bc7d8f5a9d93f88bcfb52b62444fd842720
talalcodes55 <talalalghazal55@gmail.com>
2025-10-27 10:23:34 +0300

    add shop by category section in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,16 @@ export default function Home() {
             
           </div>
         </div> 
-      </main>
-    </div>
+      </section>
+      <section className="flex flex-col justify-center items-center min-h-screen w-full p-4 bg-gray-100">
+          <h1 className="text-3xl text-center font-bold mb-10">Shop by category</h1>
+          <div className="grid grid-cols-3 w-full lg:w-3/4 gap-5">
+            {components.map((category) => (
+              <CategoryCard key={category.title} category={category} />
+            ))}
+          </div>
+          
+      </section>
+    </main>
   );
 }
