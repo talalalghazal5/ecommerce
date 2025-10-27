2662161c3c526efa26b417bdea42de175716476d
talalcodes55 <talalalghazal55@gmail.com>
2025-10-27 10:20:09 +0300

    added a fourth hero image to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,11 @@ export default function Home() {
         <div className="relative">
           <Image src={heroImage} alt="hero image" width={400} height={400} className="relative"></Image>
           <Image src={secondHeroImage} alt="second hero image" width={400} height={400} className="absolute top-0 right-50"></Image>
-          <Image src={thirdHeroImage} alt="second hero image" width={300} height={300} className="absolute bottom-[-10] right-40"></Image>
+          <Image src={thirdHeroImage} alt="second hero image" width={300} height={300} className="absolute bottom-[-10] left-30"></Image>
+          <Image src={fourthHeroImage} alt="second hero image" width={300} height={300} className="absolute bottom-[-10] right-40"></Image>
+          <div className="w-100 h-5 absolute rounded-full blur-xl bg-gray-300 right-10">
+            
+          </div>
         </div> 
       </main>
     </div>
