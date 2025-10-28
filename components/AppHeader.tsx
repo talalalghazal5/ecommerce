31f4ff5e39a416c83c7a8db2c2e9b7a4c97d4e1c
talalcodes55 <talalalghazal55@gmail.com>
2025-10-28 12:28:41 +0300

    initialize cart sheet

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -6,6 +6,50 @@ import 'boxicons/css/boxicons.min.css'
 import AppLogo from './AppLogo'
 
 
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
+export function SheetDemo() {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button size={'icon-lg'} className='rounded-full cursor-pointer bg-transparent hover:bg-muted'><i className='bx bx-cart text-2xl text-black' ></i></Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>My cart</SheetTitle>
+                </SheetHeader>
+                <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                    <div className="grid gap-3">
+                        {/* <Label htmlFor="sheet-demo-name">Name</Label>
+                        <Input id="sheet-demo-name" defaultValue="Pedro Duarte" /> */}
+                    </div>
+                    <div className="grid gap-3">
+                        {/* <Label htmlFor="sheet-demo-username">Username</Label>
+                        <Input id="sheet-demo-username" defaultValue="@peduarte" /> */}
+                    </div>
+                </div>
+                <SheetFooter>
+                    <Button type="submit">Save changes</Button>
+                    <SheetClose asChild>
+                        <Button variant="outline">Close</Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+
+
 export default function AppHeader() {
     return (
         <header className='px-6 py-3 flex items-center backdrop-blur-3xl z-50 justify-evenly fixed w-full border-b'>
@@ -13,9 +57,13 @@ export default function AppHeader() {
             <Navbar />
             <Searchbar />
             <div className="flex gap-7">
-                <Button size={'icon-lg'} className='rounded-full cursor-pointer bg-transparent hover:bg-muted'><i className='bx bx-cart text-2xl text-black' ></i></Button>
+                
+                <SheetDemo />
                 <Button size={'icon-lg'} className='rounded-full cursor-pointer bg-transparent hover:bg-muted'><i className='bx bx-user text-2xl text-black' ></i></Button>
             </div>
         </header>
     )
 }
+
+
+
