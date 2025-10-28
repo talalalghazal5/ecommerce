d255daffc9bcaf21ff6b7fe619f2e9ce36af3462
talalcodes55 <talalalghazal55@gmail.com>
2025-10-28 14:47:54 +0300

    create cart context

---
+++ contexts/CartContext.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { CartItem } from '@/data/types';
+import React, { useContext } from 'react'
+
+type CartContextType = {
+    cart: CartItem[];
+    addToCart: (item: CartItem) => void;
+    removeFromCart: (id: string) => void;
+    updateQuantity: (id: string, quantity: number) => void;
+}
+
+const CartContext = React.createContext<CartContextType>({
+    cart: [],
+    addToCart: () => { },
+    removeFromCart: () => { },
+    updateQuantity: () => { },
+});
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+    const [cart, setCart] = React.useState<CartItem[]>([]);
+
+    const addToCart = (item: CartItem) => {
+        setCart((prevCart) => {
+            const existing = prevCart.find(i => i.id === item.id);
+            if (existing) {
+                // merge by increasing quantity
+                return prevCart.map(i => i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i);
+            }
+            return [...prevCart, item];
+        });
+    };
+    const removeFromCart = (id: string) => {
+        setCart(prev => prev.filter(item => item.id !== id));
+    }
+    const updateQuantity = (id: string, quantity: number) => {
+        setCart(prev => prev.map(item => item.id === id ? { ...item, quantity } : item));
+    }
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, updateQuantity }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export const useCart = () => useContext(CartContext);
