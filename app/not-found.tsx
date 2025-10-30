7aed74b1474583e6bc1f2a510841b44f4bfea3f1
talalcodes55 <talalalghazal55@gmail.com>
2025-10-30 22:57:45 +0300

    fix: correct function name and enhance Not Found page with navigation button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,17 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
-export default function notFoundPage() {
+export default function NotFoundPage() {
+    const router = useRouter();
     return (
         <main className='flex justify-center items-center min-h-screen bg-background'>
             <div className="flex flex-col items-center">
                 <h1 className='text-9xl text-center font-bold text-primary'>404</h1>
                 <h2 className='text-3xl font-semibold text-gray-600'>Page not found!</h2>
                 <p className='text-lg text-gray-500 mt-3'>Oops.. It seems that what you are looking for is not found</p>
+                <Button size={'lg'} onClick={() => router.replace('/')} className='mt-3 cursor-pointer'>Go back</Button>
             </div>
         </main>
     )
