7118e985dcd0daebe2d3fe35c51aa263f5809c12
talalcodes55 <talalalghazal55@gmail.com>
2025-10-31 15:26:40 +0300

    refine the signin button

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,11 +2,13 @@
 import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function SigninPage() {
     return (
         <div className='min-h-screen flex justify-center items-center'>
-            <Button onClick={() => signIn('github')}>sign in</Button>
+            <Button onClick={() => signIn('github', {redirectTo: "/"})}><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> Continue with GitHub</Button>
         </div>
     )
 }
