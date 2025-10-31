284b311dff86198b5eaa6c7c74778dc77b7d2ede
talalcodes55 <talalalghazal55@gmail.com>
2025-10-31 11:10:00 +0300

    create a component for the user image

---
+++ components/main/ui/UserAvatar.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { useSession} from 'next-auth/react';
+import Image from "next/image";
+import { auth } from '@/auth';
+
+
+export default async function UserAvatar() {
+    const session= await auth()
+    if(!session?.user?.name) return null
+    return (
+        <Image src={session.user.image!} alt='' width={40} height={40} className='rounded-full'></Image>
+    )
+}
