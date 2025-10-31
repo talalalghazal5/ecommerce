cedab64fb4cac4cec19d1dc976918ff8af321165
talalcodes55 <talalalghazal55@gmail.com>
2025-10-31 17:35:33 +0300

    rename middleware to proxy due to new Next js guidelines

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
-export {auth as middleware } from '@/auth'
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  if (!req.cookies.get("authjs.session-token")?.value) {
+    return NextResponse.redirect(new URL("/signin", req.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: ["/"],
+};
