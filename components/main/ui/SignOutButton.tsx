327f646c80b5c5517fa023b8ac47d74e41a6eec3
talalcodes55 <talalalghazal55@gmail.com>
2025-10-31 11:09:41 +0300

    create a button for signing out

---
+++ components/main/ui/SignOutButton.tsx
@@ -0,0 +1,11 @@
+import { handleSignOut } from '@/actions/auth-actions';
+import { signOut } from '@/auth';
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function SignOutButton() {
+
+    return (
+    <Button  onClick={handleSignOut}>Sign out</Button>
+    )
+}
