f32d62a2868ccfa4979558b11696abd35331b5df
talalcodes55 <talalalghazal55@gmail.com>
2025-11-01 13:18:26 +0300

    code refine

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: false,
     remotePatterns: [
-      { protocol: 'https', hostname: '**.example-cdn.com', pathname: '/**' },
-      {protocol: 'https', hostname: 'avatars.githubusercontent.com'},
-      {protocol: 'https', hostname: 'lh3.googleusercontent.com'}
+      { protocol: "https", hostname: "**.example-cdn.com", pathname: "/**" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
   reactCompiler: true,
