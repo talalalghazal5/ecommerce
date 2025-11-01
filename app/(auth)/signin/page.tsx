be0219b06aff04afd7fb613e452e2ce8bf0055e4
talalcodes55 <talalalghazal55@gmail.com>
2025-11-01 15:57:15 +0300

    display buttons in vertical alignments

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,7 +4,7 @@ import SinginWithGoogleButton from '../components/SinginWithGoogleButton'
 
 export default function SigninPage() {
     return (
-        <div className='min-h-screen flex justify-center items-center'>
+        <div className='min-h-screen flex flex-col space-y-3 justify-center items-center'>
             <SigninButton></SigninButton>
             <SinginWithGoogleButton />
         </div> 
