50fad7aa247bc1937c876e3fb55b85405126fe4f
talalcodes55 <talalalghazal55@gmail.com>
2025-11-03 10:10:29 +0300

    prevent the user from going back to auth page after signing in

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  if (!req.cookies.get("authjs.session-token")?.value) {
-    return NextResponse.redirect(new URL("/signin", req.nextUrl));
+  if (req.cookies.get("authjs.session-token")?.value) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/signin"],
 };
