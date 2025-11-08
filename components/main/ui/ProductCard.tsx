7856ff0da3ac9cfd5c6b228e019b4ec8661ef95d
talalcodes55 <talalalghazal55@gmail.com>
2025-11-08 16:14:21 +0300

    remove unused code

--- components/main/ui/ProductCard.tsx
+++ components/main/ui/ProductCard.tsx
@@ -10,42 +10,13 @@ import { useCart } from '@/contexts/CartContext';
 import DeletionAlert from '../cart/DeletionAlert';
 import { useRouter } from 'next/navigation';
 
-type QuantitySelectorProps = {
-    quantity: number;
-    onIncrement: () => void;
-    onDecrement: () => void;
-    isProductCard?: boolean;
-}
-
-export const QuantitySelector = ({ quantity, onIncrement, onDecrement, isProductCard }: QuantitySelectorProps) => {
-    return (
-        <div className="flex">
-            {quantity === 1 && !isProductCard ? <DeletionAlert onClick={onDecrement}><Button size={'icon-sm'} variant={'outline'} className='rounded-br-none rounded-tr-none cursor-pointer'><FontAwesomeIcon icon={faMinus}></FontAwesomeIcon></Button>
-            </DeletionAlert> : <Button size={'icon-sm'} variant={'outline'} onClick={onDecrement} className='rounded-br-none rounded-tr-none'><FontAwesomeIcon icon={faMinus}></FontAwesomeIcon></Button>
-            }
-            <div className="w-[30px] text-center flex justify-center items-center border">{quantity}</div>
-            <Button size={'icon-sm'} variant={'outline'} onClick={onIncrement} className='rounded-bl-none rounded-tl-none cursor-pointer'><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></Button>
-        </div>
-    )
-}
 
 export default function ProductCard({ product }: { product: Product, width?: number, height?: number }) {
     const [quantity, setQuantity] = useState<number>(1);
     const cart = useCart();
     const router = useRouter();
 
-    const handleQtyIncrement = () => {
-        setQuantity(prev => prev + 1);
-    }
 
-    const handleQtyDecrement = () => {
-        setQuantity(prev => Math.max(1, prev - 1));
-    }
-    const handleAddToCart = () => {
-        const cartItem = { id: product.id, product: product, quantity: quantity };
-        if (cart.cart.includes(cartItem)) setQuantity(quantity + 1)
-        cart.addToCart(cartItem);
-    }
 
     const handleTransition = () => {
         router.push(`/product/${product.id}`)
