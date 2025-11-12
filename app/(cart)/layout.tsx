bd9235f4bae45d99523cd494a2a4a19edd5aab6c
talalcodes55 <talalalghazal55@gmail.com>
2025-11-12 12:35:48 +0300

    remove providers from sub layouts

--- app/(cart)/layout.tsx
+++ app/(cart)/layout.tsx
@@ -5,28 +5,26 @@ import React from 'react'
 
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const rougeScript = Rouge_Script({
-  variable: "--font-rouge-script",
-  subsets: ["latin"],
-  weight: ["400"]
+    variable: "--font-rouge-script",
+    subsets: ["latin"],
+    weight: ["400"]
 })
 
 
 export default function CartLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    
+
     return (
-        <html lang='en'>
-            <SessionProvider>
-                <CartProvider>
-                    <body className={`${geistSans.className} ${rougeScript.variable} antialiased`}>
-                        {children}
-                    </body>
-                </CartProvider>
-            </SessionProvider>
-        </html>
+
+
+        <div className={`${geistSans.className} ${rougeScript.variable} antialiased`}>
+            {children}
+        </div>
+
+
     )
 }
