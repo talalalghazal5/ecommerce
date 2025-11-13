776ff18faed670ecb2bcefb932fd02768933c42b
talalcodes55 <talalalghazal55@gmail.com>
2025-11-13 11:23:40 +0300

    create checkout button component

---
+++ app/(marketing)/cart/components/CheckoutButton.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function CheckoutButton() {
+    return (
+        <Button className='w-full cursor-pointer'>Continue to checkout</Button>
+    )
+}
