bc8695099e6f454687da7f7629f428af0953e2bd
talalcodes55 <talalalghazal55@gmail.com>
2025-11-15 19:23:17 +0300

    apply grid background for the landing section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,10 +3,19 @@ import HeroImage from "@/components/main/HeroImage";
 import CategoriesGrid from "@/components/main/CategoriesGrid";
 import ProductsGrid from "@/components/main/ProductsGrid";
 import NewsletterForm from "@/components/main/NewsletterForm";
+import { InteractiveGridPattern } from "@/components/ui/interactive-grid-pattern";
+import { cn } from "@/lib/utils";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center ">
       <section className="flex min-h-screen w-full items-center justify-around">
+        <InteractiveGridPattern
+          className={cn(
+            "[mask-image:radial-gradient(1000px_circle_at_left,white,transparent)]", "z-[-1]"
+          ) } width={40}
+          height={40}
+          squares={[80, 80]}
+          squaresClassName="hover:fill-blue-500"/>
         <Slogan />
         <HeroImage />
       </section>
