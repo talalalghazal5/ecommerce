2b26bf23c46c4ee87a0ac5451c528a7d407af880
talalcodes55 <talalalghazal55@gmail.com>
2025-11-16 14:12:23 +0300

    redecorate text effects in landing section

--- components/main/Slogan.tsx
+++ components/main/Slogan.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from '../ui/button'
+import { AuroraText } from '../ui/aurora-text'
 
 export default function Slogan() {
     return (
         <div>
-            <h1 className="text-5xl font-bold">Shopping became <span className="font-rouge text-7xl text-amber-500">premium.</span></h1>
+            <h1 className="text-5xl font-bold">Shopping became <AuroraText colors={['oklch(76.9% 0.188 70.08), oklch(66.6% 0.179 58.318)']}>premium.</AuroraText></h1>
             <h2 className="mt-3 mb-4 max-w-2xl text-md text-gray-700">
                 Discover a truly elevated shopping experience — curated collections handpicked for quality and style, effortless one-click checkout.
             </h2>
