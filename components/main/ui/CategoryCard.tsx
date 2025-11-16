b1ea49ad82faced7dc9244851167fd8b63ea44ae
talalcodes55 <talalalghazal55@gmail.com>
2025-11-16 14:02:42 +0300

    redecorated category card

--- components/main/ui/CategoryCard.tsx
+++ components/main/ui/CategoryCard.tsx
@@ -12,25 +12,28 @@ import { Category } from "@/data/types"
 import 'boxicons/css/boxicons.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
+import { ShineBorder } from "@/components/ui/shine-border";
+import { RainbowButton } from "@/components/ui/rainbow-button";
 
 export function CategoryCard({ category }: { category: Category }) {
     const router = useRouter();
     return (
-        <Card className={`w-full border-b-4 border-${category.color}`}>
+        <Card className={`w-full border-b-4 relative`}>
+            {/* <ShineBorder borderWidth={2} shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} /> */}
             <CardHeader>
-                <CardTitle className={`text-${category.color} text-lg`}>{category.title}</CardTitle>
+                <CardTitle className={`text-lg`}>{category.title}</CardTitle>
                 <CardDescription className="text-black">
                     Enter your email below to login to your account
                 </CardDescription>
                 <CardAction className="text-6xl h-full flex items-center">
                     {
-                        <FontAwesomeIcon className={`text-${category.color}`} icon={category.icon!}/>
+                        <FontAwesomeIcon icon={category.icon!} />
                     }
-                </CardAction>   
+                </CardAction>
             </CardHeader>
             <CardContent>
-                <Button className={`mt-4 bg-${category.color} cursor-pointer`} onClick={() => router.push(`/categories/${category.id}`)}>Shop now</Button>
+                <RainbowButton variant={'outline'} onClick={() => router.push(`/categories/${category.id}`)}>Shop now</RainbowButton>
             </CardContent>
-        </Card> 
+        </Card>
     )
 }
