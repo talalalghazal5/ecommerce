028bfe7d2651aa0781a40d05a54f407712cd792c
talalcodes55 <talalalghazal55@gmail.com>
2025-11-28 14:17:09 +0300

    add styling to the not found page in the marketing route group

--- app/(marketing)/not-found.tsx
+++ app/(marketing)/not-found.tsx
@@ -1,18 +1,16 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { useRouter } from 'next/navigation'
+import { MorphingText } from '@/components/ui/morphing-text'
+import { TextAnimate } from '@/components/ui/text-animate'
 import React from 'react'
 
-export default function NotFoundPage() {
-    const router = useRouter();
+export default function NotFound() {
     return (
-        <main className='flex justify-center items-center min-h-screen bg-background'>
-            <div className="flex flex-col items-center">
-                <h1 className='text-9xl text-center font-bold text-primary'>404</h1>
-                <h2 className='text-3xl font-semibold text-gray-600'>Page not found!</h2>
-                <p className='text-lg text-gray-500 mt-3'>Oops.. It seems that what you are looking for is not found</p>
-                <Button size={'lg'} onClick={() => router.replace('/')} className='mt-3 cursor-pointer'>Go back</Button>
-            </div>
+        <main className='flex flex-col space-y-3 justify-center items-center min-h-screen'>
+            <MorphingText texts={['404', 'Not Found']}></MorphingText>
+            <h3 className='text-2xl font-semibold'>Page not found!</h3>
+            <p className='text-zinc-500'>the page you are looking for is not found</p>
+            <Button>Go home</Button>
         </main>
     )
 }
